2266de4b6c5f8a6e8cd733504530da539deaccf0
Daniho100 <daniandre.dah@gmail.com>
2025-11-18 14:08:22 +0100

    done with task, deploying

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-const paymentInstructionsRouter = require('./routes/paymentRoutes');
+const paymentInstructionsRouter = require('./src/routes/paymentRoutes');
 
 
 app.use(express.json());
